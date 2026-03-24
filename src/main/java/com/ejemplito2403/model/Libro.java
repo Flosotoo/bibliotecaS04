48a017c8224a9656e13e32366ca866f27c33fbd3
Flosotoo <flor.soto@duocuc.cl>
2026-03-24 14:14:11 -0300

    Clase libro completa

---
+++ src/main/java/com/ejemplito2403/model/Libro.java
@@ -0,0 +1,19 @@
+package com.ejemplito2403.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Libro {
+    private int id;
+    private String isbn;
+    private String libro;
+    private String titulo;
+    private String editorial;
+    private int fechaPublicacion;
+    private String autor;
+
+
+}
